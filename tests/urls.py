28c9777fb38b219f2e0118da83a2dea9a3e9b453
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2022-07-27 20:24:28 -0300

    issue #20: tested for python 3.10 and django 3.2, 4

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('djmonaco.urls', namespace='djmonaco')),
+    path('admin/', admin.site.urls),
+    path('', include('djmonaco.urls', namespace='djmonaco')),
 ]
